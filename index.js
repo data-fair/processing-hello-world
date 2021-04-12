b5ab8d58398514488c4c4beccf41d6d65a1ce3d9
Alban Mouton <alban.mouton@gmail.com>
2021-04-12 15:31:04 +0200

    feat: implement run method with full context in test

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
 exports.pluginConfigSchema = async () => ({
   type: 'object',
   properties: {
-    defaultMessage: {
-      type: 'string',
-      title: 'Default message',
-      default: 'Hello world'
-    }
+    defaultMessage: { type: 'string', title: 'Default message', default: 'Hello world' }
   }
 })
 
 exports.processingConfigSchema = async (pluginConfig) => ({
   type: 'object',
+  required: ['dataset', 'message'],
   properties: {
-    message: {
-      type: 'string',
-      title: 'Message',
-      default: pluginConfig.defaultMessage
-    }
+    dataset: {
+      type: 'object',
+      require: ['id', 'title'],
+      properties: {
+        id: { type: 'string', title: 'Identifiant', default: 'hello-world' },
+        title: { type: 'string', title: 'Titre', default: pluginConfig.defaultMessage },
+        overwrite: { type: 'boolean', title: 'Surcharger un jeu existant', default: false }
+      }
+    },
+    message: { type: 'string', title: 'Message', default: pluginConfig.defaultMessage }
   }
 })
 
-exports.run = async (pluginConfig, processingConfig, { tmpDir }) => {
-  return {
-    schema: [{ key: 'message', type: 'string' }],
-    bulkLines: [{ _id: 'hello', message: processingConfig.message }]
+const datasetSchema = [{ key: 'message', type: 'string' }]
+
+exports.run = async ({ pluginConfig, processingConfig, processingId, tmpDir, axios, log }) => {
+  log.step('Vérification du jeu de données')
+  log.info(`tentative de lecture du jeu ${processingConfig.dataset.id}`)
+  try {
+    const dataset = (await axios.get(`api/v1/datasets/${processingConfig.dataset.id}`)).data
+    log.debug('détail du jeu de données', dataset)
+    if (dataset.extras && dataset.extras.processingId === processingId) {
+      log.info('le jeu de données existe et est rattaché à ce traitement')
+    } else {
+      if (processingConfig.dataset.overwrite) {
+        log.warning('le jeu de données existe et n\'est pas rattaché à ce traitement, l\'option "Surcharger un jeu existant" étant active le traitement peut continuer.')
+        await axios.patch(`api/v1/datasets/${processingConfig.dataset.id}`, {
+          extras: { ...dataset.extras, processingId }
+        })
+      } else {
+        throw new Error('le jeu de données existe et n\'est pas rattaché à ce traitement')
+      }
+    }
+    await axios.patch(`api/v1/datasets/${processingConfig.dataset.id}`, {
+      title: processingConfig.dataset.title,
+      schema: datasetSchema
+    })
+  } catch (err) {
+    if (!err.response || err.response.status !== 404) throw err
+    log.info('le jeu de données n\'existe pas encore')
+
+    log.step('Création du jeu de données')
+    const createdDataset = (await axios.put(`api/v1/datasets/${processingConfig.dataset.id}`, {
+      title: processingConfig.dataset.title,
+      isRest: true,
+      schema: datasetSchema,
+      extras: { processingId }
+    })).data
+    log.info('Le jeu a été créé')
+    log.debug('jeu de données créé', createdDataset)
   }
+
+  log.step('Écriture du message de bienvenue')
+  await axios.put(`api/v1/datasets/${processingConfig.dataset.id}/lines/hello`, {
+    message: processingConfig.message
+  })
+  log.info('1 ligne de donnée écrite')
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
+process.env.NODE_ENV = 'test'
+const config = require('config')
+const axios = require('axios')
+const chalk = require('chalk')
 const assert = require('assert').strict
 const helloWorld = require('../')
 
@@ -10,5 +14,31 @@ describe('Hello world processing', () => {
   it('should expose a processings config schema builder for users', async () => {
     const schema = await helloWorld.processingConfigSchema({ defaultMessage: 'Hello' })
     assert.equal(schema.properties.message.default, 'Hello')
+  })
+
+  it('should run a task', async function () {
+    this.timeout(10000)
+
+    const axiosInstance = axios.create({
+      baseURL: config.dataFairUrl,
+      headers: { 'x-apiKey': config.dataFairAPIKey }
+    })
+    await helloWorld.run({
+      pluginConfig: {},
+      processingConfig: {
+        dataset: { id: 'hello-world-test', title: 'Hello world test', overwrite: false },
+        message: 'Hello world test !'
+      },
+      axios: axiosInstance,
+      log: {
+        step: (msg) => console.log(chalk.blue.bold.underline(msg)),
+        error: (msg, extra) => console.log(chalk.red.bold(msg), extra),
+        warning: (msg, extra) => console.log(chalk.red(msg), extra),
+        info: (msg, extra) => console.log(chalk.blue(msg), extra),
+        debug: (msg, extra) => console.log(msg, extra)
+      }
+    })
+    const dataset = (await axiosInstance.get('api/v1/datasets/hello-world-test')).data
+    assert.equal(dataset.title, 'Hello world test')
   })
 })
